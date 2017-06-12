e744b3e68c0363cb264d8f67496ce911789e76a8
Stephen Fox <sfoxorama@gmail.com>
2017-06-11 21:22:48 -0400

    separate router into separate file

--- app/models/album.js
+++ app/models/album.js
@@ -5,19 +5,34 @@ mongoose.connect('mongodb://localhost/albums');
 let Schema = mongoose.Schema;
 
 let AlbumSchema = new Schema({
-    artist: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    year: {
-        type: Number,
-        required: true
-    }
+  artist: {
+    type: String,
+    required: false
+  },
+  title: {
+    type: String,
+    required: false
+  },
+  year: {
+    type: Number,
+    required: false
+  },
+  country: {
+    type: String,
+    required: false
+  },
+  catalogNumber: {
+    type: String,
+    required: false
+  },
+  discogsID: {
+    type: String,
+    required: false,
+    unique: false
+  },
+  spotifyID: {
+    type: String
+  }
 
 });
 
